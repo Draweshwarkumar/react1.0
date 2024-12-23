a0063dbb5c6e5c35492159be8c9edcd7a6719283
Draweshwarkumar <shivamjha22948@gmail.com>
2024-12-23 15:35:44 +0530

    .

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from 'react';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [Password, setPassword] = useState('');
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (numberAllowed) str += '0123456789';
+    if (charAllowed) str += '@#$%^&*-_+=[]{}~\'`';
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length); // Corrected logic
+      pass += str.charAt(char); // Append character to pass
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
 
   return (
     <>
-     <h1 className='text-4xl text-center'>Password generator</h1>
+      <div
+        className="w-full max-w-md mx-auto shadow-md 
+        rounded-lg px-4 my-8 text-orange-500 bg-gray-700"
+      >
+        test
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
