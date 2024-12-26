0c7348d539781863c5db165249e609f115d2dee3
Draweshwarkumar <shivamjha22948@gmail.com>
2024-12-26 16:32:03 +0530

    talwindprops

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback,useEffect,useRef } from 'react'
 
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharacterAllowed] = useState(false);
   const [password, setPassword] = useState("")
+
+const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() =>{
     let pass = ""
@@ -15,7 +17,7 @@ function App() {
 
     for(let i=1; i<=length; i++){
       let char = (Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
 
     }
 
@@ -23,6 +25,15 @@ function App() {
 
   }, [length,numberAllowed, charAllowed,setPassword])
 
+  const copyPasswordToClipboard = useCallback(() =>{
+    passwordRef.current?.select();
+    passwordRef.current?.setselectionRange(0,100);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(() =>{
+    passwordGenerator()
+  },[length, numberAllowed, charAllowed,passwordGenerator])
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 bg-gray-800 text-orange-500'>
@@ -34,8 +45,10 @@ function App() {
           className="outline-none w-full py-1 px-3"
           placeholder="Password"
           readOnly
+          ref={passwordRef}
           />
           <button
+          onClick={copyPasswordToClipboard}
           className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
           >copy</button>
 
