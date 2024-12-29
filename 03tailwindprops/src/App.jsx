152f0cfd4b865b5def17f3cd3e89e53cca210995
Draweshwarkumar <shivamjha22948@gmail.com>
2024-12-29 22:23:35 +0530

    Update App.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-       <h1 className='bg-green-400 text-black p-4 rounded-xl'>
+       {/* <h1 className='bg-green-400 text-black p-4 rounded-xl'>
       Tailwind test
-    </h1>
+    </h1> */}
    
     </>
   )
