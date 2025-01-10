ef78bb7cb43913b609b09ee6c7e3ee1c110e6715
Draweshwarkumar <shivamjha22948@gmail.com>
2025-01-10 18:41:20 +0530

    .

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,8 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
+
+
+
 
 const router = createBrowserRouter([
   {
